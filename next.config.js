6be11925c291eb124a4f7a76b7a21efb5c2f73b2
Omar <omarsaro2026@gmail.com>
2026-07-28 13:56:40 +0300

    fix: bypass eslint build error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextconfig: NextConfig = {
+const nextConfig = {
   eslint: {
-    // يمنع أخطاء الـ ESLint من إيقاف عملية الـ Build على Vercel
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextconfig;
+export default nextConfig;
